3f44d7ee639b1c886e27b467b5b452d1772c41c2
Jesse Herrera <tuu07965@temple.edu>
2025-07-05 23:52:21 -0400

    Added Joke Controller. Closes #3

---
+++ src/main/java/guru/springframework/controllers/JokeController.java
@@ -0,0 +1,32 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 07/05/2025 by Jesse H.
+ * Package: Controllers; Class: JokeController
+ * Model View Controller for Joke App
+ */
+
+@Controller
+public class JokeController {
+
+    // DI for JokeService
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    } // End JokeController()
+
+    @RequestMapping("/")
+    public String getJoke(Model model) {
+
+        // Adds joke attribute to the model
+        model.addAttribute("joke", jokeService.sayJoke());
+
+        return "index"; // Returns the view
+    } // End getJoke()
+} // End JokeController Class
