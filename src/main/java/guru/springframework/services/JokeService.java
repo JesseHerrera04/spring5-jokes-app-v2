fe7100cd6834b6c2c8151caa0896f5dae1d59279
Jesse Herrera <tuu07965@temple.edu>
2025-07-05 23:23:51 -0400

    Added service for Joke (interface and implementation class)

---
+++ src/main/java/guru/springframework/services/JokeService.java
@@ -0,0 +1,13 @@
+package guru.springframework.services;
+
+/**
+ * Created on 07/05/2025 by Jesse H.
+ * Package: Services; Class: JokeService
+ * Interface for implementing JokeService
+ */
+
+public interface JokeService {
+
+    String sayJoke(); // Outputs joke string
+
+} // End JokeService
---
+++ src/main/java/guru/springframework/services/JokeServiceImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created on 07/05/2025 by Jesse H.
+ * Package: Services; Class: JokeServiceImpl
+ * Class implementing JokeService Interface
+ */
+
+@Profile({"chuck", "default"})
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    /// Dependency Injection for Chuck Norris Quotes
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    } // End JokeServiceImpl()
+
+    // Overridden method prints out Chuck Norris Quote as a joke
+    @Override
+    public String sayJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    } // End sayJoke()
+} // End JokeServiceImpl Class
